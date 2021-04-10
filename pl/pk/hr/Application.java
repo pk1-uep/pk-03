3c7e79ad9ab9b88bb500915fb57137d670f4e2cb
pk1-uep <82241629+pk1-uep@users.noreply.github.com>
2021-04-10 13:40:30 +0200

    Update Application.java

--- pl/pk/hr/Application.java
+++ pl/pk/hr/Application.java
@@ -9,16 +9,22 @@ public class Application {
         Employee kowal = new Employee("Ewa", "Aleksandra", "Kowalska", 6700, 1999);
         Employee kroll = new Employee("Maria", "Anna", "Zięba", 2700, 2019);
 
+        // nowak
         System.out.println(nowak.getFullName());
         System.out.println(" - wynagrodzenie: " + nowak.getSalary() + " zł");
         System.out.println(" - do wypłaty: " + nowak.getNetSalary() + " zł");
         System.out.println(" - staż pracy: " + nowak.getHirePeriod());
 
+        // zmiana składki zdrowotnej
+        Employee.setHealth(11.0);    // od tego momentu wszystkie obiekty klasy Employee "widzą" nową wartość atrybutu "health"
+
+        // kowal
         System.out.println(kowal.getFullName());
         System.out.println(" - wynagrodzenie: " + kowal.getSalary() + " zł");
         System.out.println(" - do wypłaty: " + kowal.getNetSalary() + " zł");
         System.out.println(" - staż pracy: " + kowal.getHirePeriod());
 
+        // kroll
         System.out.println(kroll.getFullName());
         System.out.println(" - wynagrodzenie: " + kroll.getSalary() + " zł");
         System.out.println(" - do wypłaty: " + kroll.getNetSalary() + " zł");
